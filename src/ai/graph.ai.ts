6932ababc552e5bb8764a7d1785ec51abe470479
Celestial <celestialcreator11@gmail.com>
2026-09-22 00:45:03 +0530

    ai models & config

---
+++ src/ai/graph.ai.ts
@@ -0,0 +1,4 @@
+import {StateGraph, StateSchema, type GraphNode} from "@langchain/langgraph"
+
+//whenever one node wants to send data to other node the date needs to be in a proper format and this format is defined by STATESCHEMA
+
